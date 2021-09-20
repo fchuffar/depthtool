df89a13adcfc92a2fe64be19c246b02365f31be9
Florent Chuffart <florent.chuffart@gmail.com>
2021-09-20 11:33:05 +0200

    Update depthtool.R

--- depthtool.R
+++ depthtool.R
@@ -1,10 +1,22 @@
 nb_reads = 40 # M of reads
 read_size = 100 # bp
 SR_or_PE = "SR"
+
+# RNAseq
+nb_reads = 40 # M of reads
+read_size = 150 # bp
+flowcell_size = 400 # M of reads
+nb_samples = 24
+SR_or_PE = "PE"
+
+
+
+
+
+# go!
 exome_size = 72*10^6 # bp
 genome_size =  3.2*10^9 # bp
 
-# go!
 if (SR_or_PE == "SR") {
   fact = 1
 } else {
