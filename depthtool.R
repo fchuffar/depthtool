68f1f0db833c4542b578dcfff8caecff08433ef7
Florent Chuffart <florent.chuffart@gmail.com>
2021-09-20 15:12:46 +0200

    Fixing SR or PE

--- depthtool.R
+++ depthtool.R
@@ -24,13 +24,41 @@ cat("\n")
 
 
 # RNAseq
+
+# If I'm not mistaken either, C = LN / G
+# C = coverage
+# L = read length
+# N = number of reads
+# G = genome size
+#
+# So in your case : (2×150)×(16,7×10⁶)÷(72×10⁶) = 69.5X
+
 flowcell_size = 400 # M of reads
 nb_samples = 24
 read_size = 150 # bp
 SR_or_PE = "PE"
+
+
+# go!
+exome_size = 72*10^6 # bp
+genome_size =  3.2*10^9 # bp
+
+if (SR_or_PE == "SR") {
+  fact = 1
+} else {
+  fact = 2
+}
+if (SR_or_PE == "SR") {
+  fact = 1
+} else {
+  fact = 2
+}
+
 nb_reads_per_sample =  flowcell_size/nb_samples
 
 depth2 = nb_reads_per_sample * 10^6 * fact * read_size 
 mean_coverage2 = depth2 / exome_size
+
+
 cat(paste0("If I'm not mistaken, a flowcell of ", signif(flowcell_size,3), "M reads for ", nb_samples, " samples gives an average of about ", signif(nb_reads_per_sample, 3), " M reads per sample. \nFor transcriptome (", exome_size/10^6, " Mb) with read size: ", SR_or_PE, read_size, ", it means a depth of ", signif(depth2,3)/10^9, "GB and a mean coverage of ~", round(mean_coverage2), "x.\n"))
 
